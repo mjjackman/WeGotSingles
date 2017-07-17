7fe33399f9a02d76fabe3db4baede1d05a4926b1
Edd Williams <edd.williams@me.com>
2017-07-17 17:24:31 +0100

    Passing feature and test files. More changes to be made

--- features/customer_occupation.feature
+++ features/customer_occupation.feature
@@ -1,12 +1,23 @@
 Feature: Customer Occupation
 
-  @wip
-  Scenario: A Customer edits their industry and occupation
+  Background:
     Given they are registered
       And they are logged in
       And there are some industries
       And they have an industry
+
+  Scenario: A Customer edits their industry
     When they visit the edit profile page
       And they select a different industry
       And they submit changes
-    Then their industry and occupation is updated
+    Then their industry is updated
+
+  @wip
+  Scenario: A Customer edits their occupation
+    Given there are some occupations
+      And they have an industry
+      And they have an occupation
+    When they visit the edit profile page
+      And they select a different occupation
+      And they submit changes
+    Then their occupation is updated
--- features/step_definitions/customer_occupation_steps.rb
+++ features/step_definitions/customer_occupation_steps.rb
@@ -11,7 +11,26 @@ When(/^they select a different industry$/) do
   select "Politics", :from => "customer_industry_id"
 end
 
-Then(/^their industry and occupation is updated$/) do
+Then(/^their industry is updated$/) do
   @customer.reload
   expect(@customer.industry.name).to eq "Politics"
 end
+
+Given(/^there are some occupations$/) do
+  @web_dev = Occupation.create(title: "Web Developer", industry_id: @tech.id)
+  @campaign = Occupation.create(title: "Campaign Manager", industry_id: @politics.id)
+  @speech = Occupation.create(title: "Speech Writer", industry_id: @politics.id)
+end
+
+Given(/^they have an occupation$/) do
+  @customer.occupation = @speech
+end
+
+When(/^they select a different occupation$/) do
+  select "Speech Writer", :from => "customer_occupation_id"
+end
+
+Then(/^their occupation is updated$/) do
+  expect(@customer.occupation.title).to eq "Speech Writer"
+end
+

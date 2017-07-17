f9a4ad076a1d21854f03b4e381beb20eb7c228ac
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-17 17:20:35 +0100

    added seed file for populating religions table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+religions = Religion.find_or_create_by([{ name: 'Agnostic', { name: 'Atheist'}, name: 'Buddhist'}, {name: 'Catholic'}, {name: 'Protestant'}, {name: 'Hindu'}, {name: 'Muslim'}])

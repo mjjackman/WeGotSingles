0683aee029f278636b3043dfe2653fbae6c09764
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-17 17:08:48 +0100

    added smoking as boolean in customer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717131904) do
+ActiveRecord::Schema.define(version: 20170717154031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170717131904) do
     t.string "username"
     t.text "bio"
     t.integer "religion_id"
+    t.boolean "smoker"
   end
 
   create_table "religions", force: :cascade do |t|

69945d730f578ed8ccc9990da2a3f1904e191c55
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-17 20:42:50 +0100

    added mount uploader profile image uploader in customer uploader

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
   belongs_to :religion, :optional => true
   accepts_nested_attributes_for :religion
+
+  mount_uploader :profile_image, ProfileImageUploader
 end

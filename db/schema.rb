a4c6c8c92b94353d7d193dd7c98f56cc33c36cb1
Edd Williams <edd.williams@me.com>
2017-07-17 14:51:16 +0100

    Migration for occupations table, relevant changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717133216) do
+ActiveRecord::Schema.define(version: 20170717134736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "industries", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "occupations", force: :cascade do |t|
+    t.string "title"
   end
 
 end

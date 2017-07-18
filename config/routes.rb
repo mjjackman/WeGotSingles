a6edd7369b4d6bd1383b0669b3056395b5c1045b
mjjackman <mjjackman2000@yahoo.co.uk>
2017-07-18 16:09:05 +0100

    changed the url to show user/:username instead of customer/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :customers, :only => [:edit, :update, :show]
+  resources :customers, :only => [:edit, :update]
+
+  resources :customers, path: 'users', param: :username
 end

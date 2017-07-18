293c2883a550bd7b96b4cec12698abc1b0f37a76
Edd Williams <edd.williams@me.com>
2017-07-18 17:11:06 +0100

    RSpec test for perc_completed method on customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   describe 'percentage completed-ness for Customer' do
     before do 
-      @customer = Customer.new(fname: "John", lname: "Smith", username: "smiffy", drinker: true)
+      @customer = Customer.create!(fname: "John", lname: "Smith", username: "smiffy", drinker: true)
     end
 
     it 'returns the profile completed-ness as an number out of 100' do

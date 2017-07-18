497bde26bf179621948d08a02e995baa1f882fc9
Edd Williams <edd.williams@me.com>
2017-07-18 16:47:24 +0100

    Created method to calculate percentage completed-ness

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,16 @@ class Customer < ApplicationRecord
   belongs_to :occupation, :optional => true
 
   enum education_level: [:GCSE, :ALevel, :Bachelors, :Masters, :PhD]
+
+  def perc_complete
+    complete = 0
+    weight = 1 / (self.attributes.count.to_f - 1)
+    
+    self.attributes.each_pair do |attr, val|
+      complete += weight if val.present?
+    end
+    
+    return (complete * 100).to_f
+  end
+
 end

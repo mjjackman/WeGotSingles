8d4f5a8ec17508f54e64b46ccbd41f3974636dc7
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-18 15:51:00 +0100

    added cucumber test for testing whether customer can upload profile image

--- features/step_definitions/profile_image_steps.rb
+++ features/step_definitions/profile_image_steps.rb
@@ -1,11 +1,16 @@
-When(/^they add an image$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^they have a profile gallery$/) do
+  @profile_gallery = @customer.galleries.create!(:name => 'profile')
 end
 
-Then(/^their profile image is updated$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^they add an image$/) do
+  page.attach_file("customer_image", Rails.root + 'spec/fixtures/files/cat_png.png') 
+end
+
+Then(/^their profile image is added to profile gallery$/) do
+  expect(@profile_gallery.images.count).to eq 1
 end
 
 Then(/^their profile image is shown on their profile page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  save_and_open_page
+  expect(page).to have_xpath("//img[contains(@src, 'cat_png.png')]")
 end
--- features/upload_image.feature
+++ features/upload_image.feature
@@ -3,9 +3,10 @@ Feature: A customer edits images
   Scenario: A customer uploads an image
     Given they are registered
       And they are logged in
+      And they have a profile gallery
     When they visit the edit profile page
       And they add an image
       And they submit changes
-    Then their profile image is updated
+    Then their profile image is added to profile gallery
       And they are redirected to their profile page
       And their profile image is shown on their profile page

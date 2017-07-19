bd58cd31b94d575026f51c3e96e0e022fffb2f33
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-19 12:15:58 +0100

    added set profile to image, to specify profile image over regular image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718113343) do
+ActiveRecord::Schema.define(version: 20170719111121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170718113343) do
   create_table "images", force: :cascade do |t|
     t.string "image"
     t.integer "gallery_id"
+    t.boolean "set_profile"
   end
 
   create_table "industries", force: :cascade do |t|

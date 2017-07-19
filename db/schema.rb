b1c4df0d4cd0f75c1e315980912cfbc8421f7ad7
mjjackman <mjjackman2000@yahoo.co.uk>
2017-07-19 12:13:28 +0100

    added a height column to customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717184505) do
+ActiveRecord::Schema.define(version: 20170719082735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170717184505) do
     t.integer "education_level"
     t.boolean "smoker"
     t.boolean "drinker"
+    t.float "height"
   end
 
   create_table "industries", force: :cascade do |t|

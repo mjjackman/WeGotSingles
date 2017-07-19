edf42a966ad8ac41248c3c7cd82be551b99c5882
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-19 19:20:54 +0100

    added test for searching by gender

---
+++ features/step_definitions/gender_steps.rb
@@ -0,0 +1,28 @@
+Given(/^there are some male customers$/) do
+  @male = Customer.create!(:fname => "Sam", :lname => "Gamgee", :username => "IHeartFrodo", :gender => "male")
+end
+
+Given(/^there are some female customers$/) do
+  @female = Customer.create!(:fname => "Elizabeth", :lname => "Windsor", :username => "GodSaveMe", :gender => "female")
+end
+
+When(/^they visit the search page$/) do
+  visit customers_search_path
+end
+
+When(/^they select a gender$/) do
+  find(:css, "#gender_ids_[value='2']").set(true)
+end
+
+When(/^they click search$/) do
+  click_on 'Search'
+end
+
+Then(/^they are redirected to the index page$/) do
+  expect(page.current_path).to eq customers_path
+end
+
+Then(/^they see only customers of the gender they selected$/) do
+  expect(page.should have_content(@female.fname))
+  expect(page.should have_no_content(@male.fname))
+end

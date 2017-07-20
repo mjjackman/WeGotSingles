bdd093f27ef10f4a22f6bb6a15c7ff0f57e112d0
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-20 10:35:01 +0100

    moved database methods out of controller after refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,4 @@ class ApplicationController < ActionController::Base
   def current_customer
     Customer.find(session["warden.user.customer.key"][0][0])
   end
-
-  def profile_gallery
-    current_customer.galleries.find_by(:name => "profile")
-  end
-
-  def set_profile_image
-    # binding.pry
-    @profile = profile_gallery.images.find_by(:set_profile => true)
-    unless @profile.nil?
-      @profile.update(:set_profile => false)
-    end
-  end 
 end
